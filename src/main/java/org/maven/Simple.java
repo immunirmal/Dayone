a97f04ddd58b68942b87adac0d197b4a009b65b4
immunirmal <nirmalimmanuvel@gmail.com>
2022-04-22 08:04:55 +0530

    added

--- src/main/java/org/maven/Simple.java
+++ src/main/java/org/maven/Simple.java
@@ -1,5 +1,8 @@
 package org.maven;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,6 +12,11 @@ public static void main(String[] args) {
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.flipkart.com/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+driver.quit();
+
+
 	
 	
 	
